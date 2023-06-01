461c5166c7b5a03de9750675ad5b25411dfb8365
Hash33m <118221605+Hash33m@users.noreply.github.com>
2023-05-31 21:42:24 -0500

    updated .env

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
   host: '127.0.0.1',
-  user: 'root',
-  password: '934310',
-  database: 'inventory_db'
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 async function viewAllEmployees() {
